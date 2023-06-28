d159b037ce466bc1fb4d801e8e95eb0110c85875
Davi Fernandes <davifncosta@gmail.com>
2023-06-28 15:10:38 -0300

    add firebase

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import { signInWithGooglePopup } from "../utils/firebase/firebase.js"
 
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup
+    console.log(response)
+  }
+
+
   return (
     <div>
       <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in With Google</button>
     </div>
   );
 };
---
+++ src/utils/firebase/firebase.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+
+import { getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider
+} from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATxhTymxR6QOnURGFwqHAWSmdZ-Sh58gU",
+  authDomain: "costa-eccomerce.firebaseapp.com",
+  projectId: "costa-eccomerce",
+  storageBucket: "costa-eccomerce.appspot.com",
+  messagingSenderId: "1067060858532",
+  appId: "1:1067060858532:web:a1051c9d12c70788476031"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider()
+
+provider.setCustomParameters({
+    prompt: "select_account"
+})
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
